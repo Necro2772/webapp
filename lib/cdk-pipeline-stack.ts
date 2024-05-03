f0fe1e2cdc925a81a9ab99467368582d3e8dd36f
Michael Roberts <50212290+Necro2772@users.noreply.github.com>
2024-05-03 08:13:02 -0700

    empty pipeline

---
+++ lib/cdk-pipeline-stack.ts
@@ -0,0 +1,24 @@
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
+import { Stack, StackProps } from 'aws-cdk-lib';
+
+export class CdkPipelineStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'MyServicePipeline',
+            synth: new ShellStep('Synth', {
+                input: CodePipelineSource.gitHub('Necro2772/webapp', 'main'),
+                installCommands: ['npm i -g npm@latest'],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ],
+            }),
+        });
+
+
+    }
+}
